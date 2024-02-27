c2413294ad859c46d33f6eed67edea816364004e
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-02-27 22:29:21 +0530

    created the MongoDB data base and connected with code. working on Schema

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("testing the blog app start");
+import express from "express";
+import mongoose from "mongoose"
+
+const app = express();
+
+mongoose.connect('mongodb+srv://AjayHrishi:WhTgD53IVEfl93fQ@blogappbackend.nsevk9k.mongodb.net/?retryWrites=true&w=majority').then(app.listen(5000))
+.then(()=>console.log('DataBase is connected')).catch((err)=>console.log(err));
+
+
+//ehjqZOlGITIwhGky 
+// password to the data base. Not allowed to access from anywhere right now. 
+// WhTgD53IVEfl93fQ password to the backend database. 
+
+// stopped at 23:00
