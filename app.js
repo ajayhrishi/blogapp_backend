4748340c463c9abb944b84e50385d0de7bef18f9
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-03-23 16:39:52 +0530

    created the enviroment variable

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ import mongoose from "mongoose"
 import router from './routes/userRoutes.js';
 import blogRouter from './routes/blogRoutes.js'
 import cors from 'cors'
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
+const db = process.env.DB
+console.log(db);
 app.use(cors());
 app.use(express.json());
 app.use('/api/user',router);
 app.use('/api/blogs',blogRouter);
-mongoose.connect('mongodb+srv://AjayHrishi:WhTgD53IVEfl93fQ@blogappbackend.nsevk9k.mongodb.net/?retryWrites=true&w=majority').then(app.listen(5000))
+mongoose.connect(db).then(app.listen(5000))
 .then(()=>console.log('DataBase is connected')).catch((err)=>console.log(err));
 
 
